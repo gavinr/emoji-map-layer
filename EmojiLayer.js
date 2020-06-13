ce1a114ed16548d07e3cc5f30d093b406521c59f
Gavin Rehkemper <gavin@gavinr.com>
2020-06-13 15:12:32 -0500

    in progress - sample

---
+++ EmojiLayer.js
@@ -0,0 +1,58 @@
+export default function EmojiLayer(BaseLayerView2D, GraphicsLayer) {
+  // PART A: custom extension of BaseLayerView2D
+  const CustomLayerView2D = BaseLayerView2D.createSubclass({
+    // constructor
+    // attach
+    // render
+    // detach
+
+    // implementation of render method in BaseLayerView2D
+    // https://developers.arcgis.com/javascript/latest/api-reference/esri-views-2d-layers-BaseLayerView2D.html#render
+    render: function (renderParameters) {
+      // state is a ViewState instance
+      // https://developers.arcgis.com/javascript/latest/api-reference/esri-views-2d-ViewState.html
+      const state = renderParameters.state;
+
+      // ctx is sometimes the convention for canvas 2d rendering context
+      const ctx = renderParameters.context;
+      // const canvas = ctx.canvas;
+
+      this.layer.graphics.forEach(function (graphic) {
+        const mapCoords = [graphic.geometry.x, graphic.geometry.y];
+        console.log("mapCoords", mapCoords);
+        // screenCoords array is modified in-place by state.toScreen()
+        const screenCoords = [0, 0];
+        state.toScreen(screenCoords, mapCoords[0], mapCoords[1]);
+
+        ctx.font = "60px serif";
+        // use these alignment properties for "better" positioning
+        ctx.textAlign = "center";
+        ctx.fillText("😂", screenCoords[0], screenCoords[1]);
+      });
+    },
+  });
+
+  // PART B: custom extension of GraphicsLayer,
+  // which relies on the CustomLayerView2D defined in PART A above
+  // NOTE: by extending from the GraphicsLayer module instead of the Layer module,
+  // we get built-in and familiar functionality for adding/removing graphics
+  const CustomLayer = GraphicsLayer.createSubclass({
+    constructor: function (a) {
+      // we can establish default properties here for the layer
+      // they can be changed when constructing an instance of the layer
+      // this.outerRadius = 90;
+      // this.innerRadius = 40;
+    },
+
+    createLayerView: function (view) {
+      if (view.type === "2d") {
+        return new CustomLayerView2D({
+          view: view,
+          layer: this,
+        });
+      }
+    },
+  });
+
+  return CustomLayer;
+}
--- main.js
+++ main.js
@@ -1,16 +1,66 @@
 import { loadModules } from "https://unpkg.com/esri-loader/dist/esm/esri-loader.js";
+import GetEmojiLayerConstructor from "./EmojiLayer.js";
 
 const createMap = async (element) => {
   var childElement = document.createElement("div");
   element.appendChild(childElement);
   // More info on esri-loader's loadModules function:
   // https://github.com/Esri/esri-loader#loading-modules-from-the-arcgis-api-for-javascript
-  const [Map, MapView] = await loadModules(["esri/Map", "esri/views/MapView"], {
-    css: true,
+  const [
+    Map,
+    MapView,
+    BaseLayerView2D,
+    GraphicsLayer,
+    Graphic,
+    webMercatorUtils,
+  ] = await loadModules(
+    [
+      "esri/Map",
+      "esri/views/MapView",
+      "esri/views/2d/layers/BaseLayerView2D",
+      "esri/layers/GraphicsLayer",
+      "esri/Graphic",
+      "esri/geometry/support/webMercatorUtils",
+    ],
+    {
+      css: true,
+    }
+  );
+
+  const initialGraphics = [
+    new Graphic({
+      geometry: webMercatorUtils.geographicToWebMercator({
+        type: "point",
+        longitude: -90.29452,
+        latitude: 38.639375,
+      }),
+      attributes: {
+        name: "Art Museum",
+      },
+    }),
+    new Graphic({
+      geometry: webMercatorUtils.geographicToWebMercator({
+        type: "point",
+        longitude: -90.1849,
+        latitude: 38.62463,
+      }),
+      attributes: {
+        name: "Gateway Arch",
+      },
+    }),
+  ];
+
+  const EmojiLayerConstructor = GetEmojiLayerConstructor(
+    BaseLayerView2D,
+    GraphicsLayer
+  );
+  const emojiLayer = new EmojiLayerConstructor({
+    graphics: initialGraphics,
   });
 
   const map = new Map({
     basemap: "streets-vector",
+    layers: [emojiLayer],
   });
 
   const viewOptions = {
