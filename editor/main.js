3994891ac2c73e96028ab43b2669c2ad2ac3cfe0
Gavin Rehkemper <gavinr@users.noreply.github.com>
2020-07-05 22:46:39 -0500

    use emojibase and add editor (#3)
    this is a breaking change

--- editor/main.js
+++ editor/main.js
@@ -1,16 +1,37 @@
 import { loadModules } from "https://unpkg.com/esri-loader/dist/esm/esri-loader.js";
-import EmojiCreateLayerView from "./EmojiLayer.js";
+import EmojiCreateLayerView from "/EmojiLayer.js";
+import { Picker } from "https://cdn.jsdelivr.net/npm/emoji-picker-element@1.0.3/index.min.js";
+
+let currentEmoji = false;
+
+const picker = new Picker({
+  locale: "en",
+});
+document
+  .querySelector("#sidebar")
+  .appendChild(picker)
+  .addEventListener("emoji-click", (event) => {
+    console.log(event.detail);
+    currentEmoji = event.detail;
+  });
+
+// const emojiConverter = new EmojiConvertor();
+// emojiConverter.replace_mode = "unified";
+// emojiConverter.allow_native = true;
 
 const createMap = async (element) => {
-  var childElement = document.createElement("div");
-  element.appendChild(childElement);
+  var split = Split(["#sidebar", "#viewDiv"], {
+    sizes: [25, 75],
+  });
+
   // More info on esri-loader's loadModules function:
   // https://github.com/Esri/esri-loader#loading-modules-from-the-arcgis-api-for-javascript
-  const [WebMap, MapView, BaseLayerView2D] = await loadModules(
+  const [WebMap, MapView, BaseLayerView2D, Graphic] = await loadModules(
     [
       "esri/WebMap",
       "esri/views/MapView",
       "esri/views/2d/layers/BaseLayerView2D",
+      "esri/Graphic",
     ],
     {
       css: true,
@@ -74,17 +95,40 @@ const createMap = async (element) => {
   );
 
   const viewOptions = {
-    container: childElement,
+    container: element,
     map: webmap,
   };
 
-  new MapView(viewOptions);
+  const mapView = new MapView(viewOptions);
+
+  mapView.on("click", (evt) => {
+    let attributes = {};
+    attributes[attribute] = currentEmoji.emoji.shortcodes[0];
+
+    let graphic = new Graphic({
+      geometry: {
+        type: "point", // autocasts as new Point()
+        longitude: evt.mapPoint.longitude,
+        latitude: evt.mapPoint.latitude,
+      },
+      attributes: attributes,
+    });
+
+    layer
+      .applyEdits({
+        addFeatures: [graphic],
+      })
+      .then((evt) => {
+        console.log("evt", evt);
+        layer.refresh();
+      });
+  });
 };
 
 window.addEventListener(
   "load",
   () => {
-    const rootElement = document.body;
+    const rootElement = document.querySelector("#viewDiv");
     createMap(rootElement);
   },
   false
